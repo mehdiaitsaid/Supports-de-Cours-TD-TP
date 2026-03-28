4c1f2c49fcfdb32e45994c74a0f13196a35c680c
mehdiaitsaid <aitsaid.mahdi@gmail.com>
2026-03-28 01:54:42 +0100

    fix pwa offline

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -93,6 +93,7 @@ const config: Config = {
                         content: '#25c2a0',
                     },
                 ],
+
                 workboxOptions: { globPatterns: ['**/*.{js,css,html,png,jpg,jpeg,svg,gif,webp,json}'], },
             },
         ]
